4fa86386d44cfe83143a4b9535792d9d4874a8f0
Jean Desbiens <desbiens.jean@hydro.qc.ca>
2015-02-05 19:47:12 -0500

    modifier le app.js pour lire la db

--- README.md
+++ README.md
@@ -10,3 +10,25 @@ Pour l'installer, simplement faire un git clone de ce repository.
 
 Pour lancer le programme faire :
 sudo pyhton speedlogger.py
+
+Installer sqlite3 et créer une base de données dans le meme répertoire
+
+sudo apt-get install sqlite3
+sqlite3 logger.db
+>>begin;
+>>create table speeds (timestamp datetime, speed numeric);
+>>commit;
+>>.quit
+
+installer node
+
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
+sudo dpkg -i node_latest_armhf.deb
+
+node -v
+node app.js
+npm install sqlite3
+npm install https://github.com/mapbox/node-sqlite3/tarball/master
+npm install express
+
+
--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-var http = require('http');
+
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('logger.db');
  
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '192.168.1.104');
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS counts (key TEXT, value INTEGER)");
+    db.run("INSERT INTO counts (key, value) VALUES (?, ?)", "counter", 0);
+});
  
-console.log('Server running at http://192.168.1.104:1337/');
+ 
+ 
+var express = require('express');
+var restapi = express();
+ 
+restapi.get('/data', function(req, res){
+    db.get("SELECT * FROM speeds", function(err, row){
+        res.json({ "count" : row.value });
+    });
+});
+ 
+restapi.post('/data', function(req, res){
+    db.run("UPDATE counts SET value = value + 1 WHERE key = ?", "counter", function(err, row){
+        if (err){
+            console.err(err);
+            res.status(500);
+        }
+        else {
+            res.status(202);
+        }
+        res.end();
+    });
+});
+ 
+ 
+restapi.listen(1337, '192.168.1.104');
+ 
+console.log("Submit GET or POST to http://192.168.1.104:1337/data");
