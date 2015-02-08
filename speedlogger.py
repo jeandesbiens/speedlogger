708c5577d298a2eecd424dd6bba7181606f1cc98
Jean Desbiens <desbiens.jean@hydro.qc.ca>
2015-02-08 15:54:25 -0500

    ajouter des prints de debug pour diagnostiquer

--- speedlogger.py
+++ speedlogger.py
@@ -3,42 +3,42 @@ import time
 import RPi.GPIO as GPIO
 import sqlite3
 
-GPIO.setmode(GPIO.BOARD)
-
 dbname = 'logger.db'
 
 lastState = False
-lastTime = time.time()
+lastTime = time.localtime()
 currTime = lastTime
 cumulDist = 0
 startTime = 0
 outStr = ""
+DEBUG = True
+
 
 # setup the pins on the raspberry pi
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(11,GPIO.IN)  # pin for signal from reed switch
 GPIO.setup(12,GPIO.OUT) # pin for the LED
 
 # rappeler au cycliste de mettre la tension sur la roue
+print " "
 print "------------------------------ "
 print "Mettre la roue sous tension"
 print "------------------------------ "
 print "Entrainement du ",time.ctime()
 
 #setup le début de session
-startTime = time.time()
+startTime = time.localtime()
  # CREATE TABLE speeds (timestamp DATETIME, speed NUMERIC);
 conn=sqlite3.connect(dbname)
 curs=conn.cursor()
 
 while True:
   currState = GPIO.input(11)
-  if currState and not lastState : 
-    #if we pass from low to high
-    currTime = time.time()
+  if currState and not lastState : #if we pass from low to high
+    currTime = time.localtime()
     dt = currTime-lastTime
-    currSpeed = 7.6104/dt  
-    #2.114m de circonference * 3600s/h / 1000 m/km
+    currSpeed = 7.6104/dt  #7.6104 is 2.114m de circonference * 3600s/h / 1000 m/km
     cumulDist += 2.114
     #output facile pour import dans Excel
     timeStr = "{:4.2f}".format((currTime-startTime)/60)+" min. "
@@ -51,6 +51,13 @@ while True:
       speedColorStr = "\033[1;42m"
     meanSpeedStr = "{:4.2f}".format((cumulDist)/(currTime-startTime))+" m/s "
     outStr = speedColorStr +speedStr + "\033[1;m" + meanSpeedStr + timeStr + distanceStr
+    if DEBUG :
+      print "currState : "+currState
+      print "lastState : "+lastState
+      print "currTime : "+currTime
+      print "dt : "+dt
+      print "currSpeed : "+currSpeed
+      
     print outStr
     # storing data to database
     #curs.execute("INSERT INTO speeds values(datetime('now'), (?))", (currSpeed,))
@@ -58,9 +65,9 @@ while True:
     #conn.commit()
     # end of storing data to database
 
- #   GPIO.output(12,False)
- #   time.sleep(0.05)
- #   GPIO.output(12,True)
+    GPIO.output(12,False)
+    time.sleep(0.05)
+    GPIO.output(12,True)
   else :
    # print "."
  #   GPIO.output(12,True)
