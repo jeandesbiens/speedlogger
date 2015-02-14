3a6cea4c8cc91ec04def2a46e93747fdf323b123
Jean Desbiens <desbiens.jean@hydro.qc.ca>
2015-02-14 13:37:31 -0500

    session culmulDistance

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ restapi.get('/stats', function(req, res){
      		}
      		else { // this is not the first row
      			// compute distance as the product of speed over time interval
-     			var dt = (row.timestamp - lastTime)/3600;	//time in seconds converted in hours (fraction of)
+     			var dt =(Date(row.timestamp).getTime()-Date(lastTime).getTime())
      			console.log('row last dt :'+' ' + row.timestamp+' ' + lastTime+' ' + dt);
      			cumulDistance = cumulDistance + (row.speed * dt);
      			lastTime = row.timestamp;
