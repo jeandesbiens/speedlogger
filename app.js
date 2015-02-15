f931ad40faf02bcfd9605d2d06c31b227b30f9a4
Jean Desbiens <desbiens.jean@hydro.qc.ca>
2015-02-15 13:37:03 -0500

    travail sur /dashboard

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ restapi.get('/dashboard', function(req, res){
 		'        <title>Tachymetre!</title>'+
 		'		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>'+
 		'		<script>'+
-		'		$.ajax("http://192.168.1.104:1337/stats", {method: "GET"}).then(function(data) {var myData = JSON.parse(data);$("#currSpeed").text(myData.lastSpeed);});'+
+		'		$.ajax("http://192.168.1.104:1337/stats", {method: "GET"}).then(function(data) {var myData = JSON.parse(data);$("#currSpeed").text(myData[0].lastSpeed);});'+
 		'		</script>'+
 		'    </head>'+ 
 		'    <body>'+
