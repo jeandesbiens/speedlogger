e3d3c97523e3925c7bbb4b03d4f5d659a98ab093
Jean Desbiens <desbiens.jean@hydro.qc.ca>
2015-02-15 11:20:36 -0500

    script fonctionne correctement, ajouter commentaires

--- turn_LED_Off.py
+++ turn_LED_Off.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 
+# script for turning the LED off in case the logger crash/stop when its on
+#
 # setup the pins on the raspberry pi
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(12,GPIO.OUT) # pin for the LED
+GPIO.setup(12,GPIO.OUT) # pin for the LED on the speedometer connector
+# setting the pin to True turns the LED off
 GPIO.output(12,True)
